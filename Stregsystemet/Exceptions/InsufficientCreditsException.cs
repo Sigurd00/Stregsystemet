1e178ea67883c222e011a0bce93db2a29babeb47
Sigurd <j.skadborg00@gmail.com>
2021-12-14 20:31:13 +0100

    Better exceptions

--- Stregsystemet/Exceptions/InsufficientCreditsException.cs
+++ Stregsystemet/Exceptions/InsufficientCreditsException.cs
@@ -6,12 +6,20 @@ namespace Stregsystemet
     [Serializable]
     public class InsufficientCreditsException : Exception
     {
+        public User? User { get; set; }
+        public Product? Product { get; set; }
         public InsufficientCreditsException()
         {
         }
 
         public InsufficientCreditsException(string? message) : base(message)
         {
+        }
+        public InsufficientCreditsException(User user, Product product) 
+            : base($"USER: {user} tried to buy PRODUCT: {product}, but didnt have enough credit")
+        {
+            User = user;
+            Product = product;
         }
 
         public InsufficientCreditsException(string? message, Exception? innerException) : base(message, innerException)
