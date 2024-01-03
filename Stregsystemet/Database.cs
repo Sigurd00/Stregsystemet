0d49dc3686401a229c912dbe6c40353951b66a10
jskad <j.skadborg00@gmail.com>
2024-01-03 22:29:22 +0100

    Database connection pool

---
+++ Stregsystemet/Database.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public class Database : IDisposable
+{
+    private readonly string _connectionString;
+    private SqlConnection _connection;
+
+    public Database(string connectionString)
+    {
+        _connectionString = connectionString;
+        _connection = new SqlConnection(_connectionString);
+        _connection.Open();
+    }
+
+    public IDbConnection GetOpenConnection()
+    {
+        if (_connection.State != ConnectionState.Open)
+        {
+            _connection.Open();
+        }
+
+        return _connection;
+    }
+
+    public void Dispose()
+    {
+        _connection?.Close();
+        _connection?.Dispose();
+    }
+}
